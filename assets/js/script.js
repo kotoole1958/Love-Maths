5aadb7bb729eb7ecc30c45d97d0cdfbd8b9df544
kotoole1958 <kieran_o_toole@hotmail.com>
2022-06-23 22:16:42 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
+© 2022 GitHub, Inc.
